ea6d687f6d30ee393d8129b60e960de4ce6b6e15
Lucy <lucy@absolucy.moe>
2021-06-21 10:21:31 -0400

    re-re-organize; no more need for build scripts!

--- scripts/build-testing.sh
+++ scripts/build-testing.sh
@@ -15,13 +15,6 @@ ln -s /Library/Lucy/Lucient.bundle/Lucient.dylib "$TARGET_DIR/Library/MobileSubs
 ln -s /Library/Lucy/Lucient.bundle/Info.plist "$TARGET_DIR/Library/MobileSubstrate/DynamicLibraries/Lucient.plist"
 ln -s /Library/Lucy/LucientPrefs.bundle "$TARGET_DIR/Library/PreferenceBundles/LucientPrefs.bundle"
 ln -s /Library/Lucy/LucientPrefs.bundle/LucientPrefs.plist "$TARGET_DIR/Library/PreferenceLoader/Preferences/LucientPrefs.plist"
-# Run the checksuminator; then strip and re-sign
-brimstone-processor \
-	process \
-	--state "$INITIAL_DIR/.brimstone-state.json" \
-	--code "$INITIAL_DIR" \
-	"$TARGET_DIR/Library/Lucy/Lucient.bundle/Lucient.dylib"
-ldid2 -S "$TARGET_DIR/Library/Lucy/Lucient.bundle/Lucient.dylib"
 # Pack the deb
 mkdir -p "$TARGET_DIR/DEBIAN"
 cp deb/control.full "$TARGET_DIR/DEBIAN/control"
