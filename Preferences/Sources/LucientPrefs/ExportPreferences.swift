b468db08d24f485063aa2c53f50b0f001f7e20e7
Lucy <lucy@absolucy.moe>
2021-06-25 13:34:41 -0400

    serialize colors as hex

--- Preferences/Sources/LucientPrefs/ExportPreferences.swift
+++ Preferences/Sources/LucientPrefs/ExportPreferences.swift
@@ -107,28 +107,21 @@ internal extension PreferencesJson {
 }
 
 extension Color: Codable {
-	enum CodingKeys: String, CodingKey {
-		case red, green, blue, alpha
-	}
-
 	public init(from decoder: Decoder) throws {
-		let container = try decoder.container(keyedBy: CodingKeys.self)
-		let r = try container.decode(Double.self, forKey: .red)
-		let g = try container.decode(Double.self, forKey: .green)
-		let b = try container.decode(Double.self, forKey: .blue)
-		let a = try container.decode(Double.self, forKey: .alpha)
+		let container = try decoder.singleValueContainer()
+		let hex = try container.decode(String.self)
 
-		self.init(.displayP3, red: r, green: g, blue: b, opacity: a)
+		guard let color = Color(hex: hex) else { throw "Invalid hex color: \(hex)" }
+		self = color
 	}
 
 	public func encode(to encoder: Encoder) throws {
-		let (r, g, b, a) = components
-
-		var container = encoder.container(keyedBy: CodingKeys.self)
-
-		try container.encode(r, forKey: .red)
-		try container.encode(g, forKey: .green)
-		try container.encode(b, forKey: .blue)
-		try container.encode(a, forKey: .alpha)
+		var container = encoder.singleValueContainer()
+		try container.encode(hexString())
 	}
+}
+
+extension String: Error {}
+extension String: LocalizedError {
+	public var errorDescription: String? { self }
 }
